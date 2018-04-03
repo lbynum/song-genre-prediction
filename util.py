6d8f3cddccdf25db02ac13d53b1ac8eaee819794
lbynum <luciusbynum@gmail.com>
2018-04-03 00:41:59 -0700

    start musixmatch visualization

--- util.py
+++ util.py
@@ -30,20 +30,46 @@ class MusixMatchData:
         self.MXMID = MXMID
         self.vocab = vocab
 
-    def write_to_pickle(self, filename, n, d):
+    def write_to_pickle(self, X_filename, genre_filename):
         '''
         Load text files into X, TID, and MXMID and store as pickled objects.
 
         Parameters
         --------------------
-            filename    -- string, filename
-            n           -- int, number of observations
-            d           -- int, number of features
+            X_filename      -- string, filename of predictors
+            genre_filename  -- string, filename of genre file
+            n               -- int, number of observations
+            d               -- int, number of features
         '''
+        # define dimensions (hard coded for now)
+        # n = 73658 # num observations in genres.csv UNION mxm_dataset_train
+        n = 191402 # num observations in genres.csv
+        d = 5000 # number of words in training data vocabulary
+
+        # load genres (the response) and associated track ID
+        y = np.empty(n, dtype=object)
+        genre_TID = np.empty(n, dtype=object)
+
+        with open(genre_filename, 'r', encoding='utf-8') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for i, row in enumerate(reader):
+                # progress update
+                if i % 1000 == 0:
+                    print(i)
+
+                # store genre and track id
+                genre_TID[i] = row[0]
+                y[i] = row[1]
+
+        # set for faster lookup time
+        tracks_to_keep = set(genre_TID)
+
+        # load predictors
         X = np.zeros((n, d))
-        TID = np.empty(n, dtype=object)
         MXMID = np.empty(n, dtype=object)
-        with open(filename, 'r') as csvfile:
+        # store other TID for joining purposes
+        mxm_TID = np.empty(n, dtype=object)
+        with open(X_filename, 'r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             num_rows_skipped = 0
             for i, row in enumerate(reader):
@@ -53,6 +79,9 @@ class MusixMatchData:
 
                 # keep track of row indices for observations
                 row_index = i - num_rows_skipped
+
+
+
                 if row[0][0] == '%':
                     # store vocabulary as array
                     vocab = np.array([row[0][1:]] + row[1:])
@@ -62,8 +91,13 @@ class MusixMatchData:
                     num_rows_skipped += 1
                     continue
 
-                # store TID and MXMID
-                TID[row_index] = row[0]
+                if row[0] not in tracks_to_keep:
+                    # skip track if we don't have a genre label for it
+                    num_rows_skipped += 1
+                    continue
+
+                # store MXMID and TID
+                mxm_TID[row_index] = row[0]
                 MXMID[row_index] = row[1]
 
                 for item in row[2:]:
@@ -79,8 +113,33 @@ class MusixMatchData:
         # X = csr_matrix(X)
         # mmwrite(pickled_data_path+'X', X)
 
+        # drop extra space for TIDs that are in genre data but not in mxm data
+        # these show up as None in mxm_TID array due to initialization
+        rows_to_keep = np.argwhere(mxm_TID != None).flatten()
+        mxm_TID = mxm_TID[rows_to_keep]
+        X = X[rows_to_keep]
+        MXMID = MXMID[rows_to_keep]
+
+        # drop labels for TIDs that are in genre data but not in mxm data
+        mxm_TID_set = set(str(s) for s in mxm_TID)
+        rows_to_keep = np.array([id in mxm_TID_set for id in genre_TID])
+        genre_TID = genre_TID[rows_to_keep]
+        y = y[rows_to_keep]
+
+        # sort genre data by TID
+        genre_sort_indices = np.argsort(genre_TID)
+        TID = genre_TID[genre_sort_indices]
+        y = y[genre_sort_indices]
+
+        # sort mxm data by TID
+        mxm_sort_indices = np.argsort(mxm_TID)
+        X = X[mxm_sort_indices]
+        MXMID = MXMID[mxm_sort_indices]
+
+
         # set as member data
         self.X = X
+        self.y = y
         self.TID = TID
         self.MXMID = MXMID
         self.vocab = vocab
@@ -92,6 +151,7 @@ class MusixMatchData:
         np.save(pickled_data_path+'MXMID', MXMID)
         np.save(pickled_data_path+'vocab', vocab)
         np.save(pickled_data_path+'X', X)
+        np.save(pickled_data_path+'y', y)
 
     def load_from_pickle(self):
         '''
@@ -108,8 +168,23 @@ class MusixMatchData:
         self.TID = np.load(pickled_data_path+'TID.npy')
         self.MXMID = np.load(pickled_data_path+'MXMID.npy')
         self.X = np.load(pickled_data_path+'X.npy')
+        self.y = np.load(pickled_data_path+'y.npy')
         # self.X = mmread(pickled_data_path + 'X.mtx')
         self.vocab = np.load(pickled_data_path+'vocab.npy')
+
+    def get_one_genre(self, genre_name):
+        '''
+        Select data specific to genre with name genre_name.
+
+        Parameters
+        --------------------
+            genre_name  -- str, name of genre to select
+
+        Returns
+        --------------------
+            X           -- numpy array, observations for given genre
+        '''
+        return self.X[self.y == genre_name]
 
 
 
--- visualize-musixmatch.py
+++ visualize-musixmatch.py
@@ -1,7 +1,6 @@
-import pickle
-
+from nltk.corpus import stopwords
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
 
 from util import MusixMatchData
 
@@ -13,12 +12,43 @@ def main():
     try:
         data.load_from_pickle()
     except:
-        data.write_to_pickle()
+        data.write_to_pickle(
+            X_filename='data/musixmatch/mxm_dataset_train.txt',
+            genre_filename='genres.csv')
+
+    ## compute total word counts
+    print('Computing total word counts...')
+    col_sums = np.sum(data.X, axis=0)
+    # add words to counts
+    word_counts = zip(data.vocab, col_sums)
+    # sort by count
+    word_counts = sorted(word_counts, key=lambda pair: pair[1], reverse=True)
+
+    ## barplot for highest occuring words
+    words = [pair[0] for pair in word_counts[:20]]
+    counts = [pair[1] for pair in word_counts[:20]]
+    y_pos = np.arange(len(words))
+    plt.bar(y_pos, counts)
+    plt.xticks(y_pos, words)
+    plt.title('Highest occuring words')
+    plt.show()
+
+    ## barplot for highest occuring words minus stopwords
+    # remove stopwords
+    stop_words = set(stopwords.words('english'))
+    word_counts = [pair for pair in word_counts
+                   if pair[0] not in stop_words]
+    words = [pair[0] for pair in word_counts[:20]]
+    counts = [pair[1] for pair in word_counts[:20]]
+    y_pos = np.arange(len(words))
+    plt.bar(y_pos, counts)
+    plt.xticks(y_pos, words)
+    plt.title('Highest occuring words without stopwords')
+    plt.show()
 
 
-
-
-
+    # TODO: also do most popular i.e. binary instead of counts
+    # TODO: look at same plot for each genre
 
 
 
