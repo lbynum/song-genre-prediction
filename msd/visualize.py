67a76816c31b07df678fc1d71afbd7a1ffe15e92
kbengtsson <kbengtsson@hmc.edu>
2018-04-03 13:36:15 -0700

    write data csv for song features

--- msd/visualize.py
+++ msd/visualize.py
@@ -42,9 +42,8 @@ sys.path.append( path )
 # imports specific to the MSD
 import hdf5_getters as GETTERS
 
-all_artist_names = set()
 # we define this very useful function to iterate the files
-def apply_to_all_files(basedir,func=lambda x: x,ext='.h5'):
+def avg_feature_all_files(basedir, genre_dict, ext='.h5'):
     """
     From a base directory, go through all subdirectories,
     find all files with the given extension, apply the
@@ -57,34 +56,56 @@ def apply_to_all_files(basedir,func=lambda x: x,ext='.h5'):
     RETURN
        number of files
     """
-    count = 0
+    features_vs_genre = pd.DataFrame()
+
     # iterate over all files in all subdirectories
     for root, dirs, files in os.walk(basedir):
         files = glob.glob(os.path.join(root,'*'+ext))
-        # count files
-        count += len(files)
+        # # count files
+        # count += len(files)
         # apply function to all files
         for f in files :
-            func(f)       
-    return count
+            h5 = GETTERS.open_h5_file_read(f)
+            song_id = GETTERS.get_track_id(h5).decode('utf-8')
+            if (song_id in genre_dict):
+                genre = genre_dict[song_id]
+                danceability = GETTERS.get_danceability(h5)
+                duration = GETTERS.get_duration(h5)
+                end_of_fade_in = GETTERS.get_end_of_fade_in(h5)
+                energy = GETTERS.get_energy(h5)
+                loudness = GETTERS.get_loudness(h5)
+                song_hotttnesss = GETTERS.get_song_hotttnesss(h5)
+                tempo = GETTERS.get_tempo(h5)
+                example = pd.DataFrame(data=[(genre, danceability, duration, 
+                                              end_of_fade_in, energy, loudness, 
+                                              song_hotttnesss, tempo)], 
+                                       columns=['genre', 'danceability', 
+                                                'duration', 'end_of_fade_in', 
+                                                'energy', 'loudness', 
+                                                'song_hotttnesss', 'tempo'])
+                features_vs_genre = features_vs_genre.append(example)
+            h5.close()
 
-def extract_features(filename):
-    h5 = GETTERS.open_h5_file_read(filename)
-    artist_name = GETTERS.get_artist_name(h5)
-    print(artist_name)
-    all_artist_names.add( artist_name )
-    h5.close()
+    return genre_vs_features
 
 def main():
     genres_data_frame = pd.read_csv(msd_code_path+'/../genres.csv', 
                                     dtype={'song_id': str, 'genre': str})
 
-    apply_to_all_files(msd_subset_data_path, func=extract_features)
-    print(len(all_artist_names))
+    unique_genres = genres_data_frame['genre'].unique()
 
+    # Create the dictionary of songs to genres.
+    genre_dict = {}
+    for col in range(genres_data_frame.shape[0]):
+        genre_dict[genres_data_frame['song_id'][col]] = genres_data_frame['genre'][col]
+        # print(genres_data_frame['song_id'][col], genres_data_frame['genre'][col])
 
+    features_vs_genre = avg_feature_all_files(msd_subset_data_path, genre_dict)
+    
+    # write csv file
+    features_vs_genre.to_csv('./features_vs_genre.csv')
 
-
+        
 
 if __name__ == "__main__":
     main()
