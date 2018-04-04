0f4241cff1b578faa8c373ada8f5081a529c2e5d
lbynum <luciusbynum@gmail.com>
2018-04-03 18:03:26 -0700

    musixmatch svm

---
+++ svm-musixmatch.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.dummy import DummyClassifier
+from sklearn.pipeline import Pipeline
+
+from util import MusixMatchData
+
+
+def main():
+    data = MusixMatchData()
+    try:
+        data.load_from_pickle()
+    except:
+        data.write_to_pickle(
+            X_filename='data/musixmatch/mxm_dataset_train.txt',
+            genre_filename='genres.csv'
+        )
+
+    parameters = {
+        'clf__C': tuple(10 ** np.arange(-3, 3))
+    }
+
+    pipeline = Pipeline([
+        ('clf', SVC('linear'))
+    ])
+
+
+    # grid search with respect to different metrics and print results
+    scoring = ['accuracy', 'precision', 'f1', 'recall', 'roc_auc']
+    for metric in scoring:
+        grid_search = GridSearchCV(
+            pipeline,
+            parameters,
+            scoring=metric,
+            n_jobs=-2,
+            verbose=1
+        )
+
+        grid_search.fit(data.X, data.y)
+
+        # print params for best fit
+        print("Best {}: {}".format(metric, grid_search.best_score_))
+        print("Best parameters set:")
+        best_parameters = grid_search.best_estimator_.get_params()
+        for param_name in sorted(parameters.keys()):
+            print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+
+if __name__ == '__main__':
+    main()
